69d06a71a17c78e5caaf28c959d7d2279bebeb49
Daniel Bernardo <danieloliveira.bernardo@gmail.com>
2014-03-13 01:06:18 -0400

    Adicionando Projeto de Player feito em c# com download de videos via ftp

---
+++ IndoorOnlinePlay/Xml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace IndoorOnlinePlay
+{
+    class Xml
+    {
+        public void createNode(string vId, string vTitulo, string vName, string vAtivo, XmlTextWriter writer)
+        {
+
+            writer.WriteStartElement("Video");
+
+
+            writer.WriteStartElement("videoId");
+
+            writer.WriteString(vId);
+
+            writer.WriteEndElement();
+
+
+            writer.WriteStartElement("VideoTitulo");
+
+            writer.WriteString(vTitulo);
+
+            writer.WriteEndElement();
+
+
+            writer.WriteStartElement("VideoName");
+
+            writer.WriteString(vName);
+
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("VideoAtivo");
+
+            writer.WriteString(vAtivo);
+
+            writer.WriteEndElement();
+
+
+            writer.WriteEndElement();
+
+        }
+
+
+ 
+
+        public void geraXML()
+        {
+            XmlTextWriter writer = new XmlTextWriter("listaLocal.xml", System.Text.Encoding.UTF8);
+
+            writer.WriteStartDocument(true);
+
+            writer.Formatting = Formatting.Indented;
+
+            writer.Indentation = 2;
+
+            writer.WriteStartElement("Table");
+
+            string id, titulo, video, ativo;
+
+            //deve ser preenchino os parâmetros de conexao ser = "hostbanco", user="userbanco","pwd="senhabanco",database = "nomeBanco"
+            MySqlConnection conexao = new MySqlConnection("server=hostbanco;user id=userbanco;pwd=senhabanco;database=nomeBanco");
+
+            //Nessa linha de comando é usado uma query para buscar os vídeos tidos como ativo no banco de dados
+            MySqlCommand sql = new MySqlCommand("select *from videos where ativo = 'S'", conexao);
+
+            try
+            {
+                conexao.Open();
+
+                MySqlDataReader ler = sql.ExecuteReader();
+
+                //Console.WriteLine("Listando dados....");
+
+                while (ler.Read())
+                {
+                    //Console.WriteLine(ler["id"] + " " + ler["titulo"] + " " + ler["video"] + " " + ler["ativo"]);
+                    //Console.WriteLine(ler.GetInt32(0) + ler.GetInt32(1) + ler.GetInt32(2) + ler.GetInt32(3));
+
+                    /*if (ler.GetInt32(1) == 1)
+                    {
+
+                        string n = ler.GetString(3);
+
+                        MessageBox.Show(n, "Resultado");
+                    }*/
+
+                    id = ler.GetInt32(0).ToString();
+                    titulo = ler.GetString(2);
+                    video = ler.GetString(3);
+                    ativo = ler.GetString(4);
+
+                     createNode(id, titulo, video, ativo, writer);
+
+                }
+
+
+                ler.Close();
+                conexao.Close();
+                //Console.ReadKey();
+
+            }
+            catch (MySqlException ex)
+            {
+
+                Console.WriteLine("Erro de conexao" + ex);
+                //Console.ReadKey();
+
+            }
+
+            //createNode("1", "Video2", "video2.avi","S", writer);
+
+            writer.WriteEndElement();
+
+            writer.WriteEndDocument();
+
+            writer.Close();
+
+            MessageBox.Show("XML File created ! ");
+
+        }
+
+
+        public void geraXMLLimpo()
+        {
+            XmlTextWriter writer = new XmlTextWriter("listaLocal.xml", System.Text.Encoding.UTF8);
+
+            writer.WriteStartDocument(true);
+
+            writer.Formatting = Formatting.Indented;
+
+            writer.Indentation = 2;
+
+            writer.WriteStartElement("Table");
+
+            //string id=null, titulo=null, video=null, ativo=null;
+
+            //createNode(id, titulo, video, ativo, writer);
+
+            //createNode("1", "Video2", "video2.avi","S", writer);
+
+            writer.WriteEndElement();
+
+            writer.WriteEndDocument();
+
+            writer.Close();
+
+            MessageBox.Show("XML File created ! ");
+
+        }
+
+      
+ 
+    }
+}
---
+++ IndoorOnlinePlay/download.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+
+namespace IndoorOnlinePlay
+{
+    class download
+    {       
+            private string localPath;
+            private string fileRemote;
+            private string nameFileSave;
+            private string folder;
+
+            public void setLocalPath(string localPath)
+            {
+                this.localPath = localPath;
+
+            }
+
+            public void setFileRemote(string fileRemote)
+            {
+                this.fileRemote = fileRemote;
+            }
+
+            public void setNameFileSave(string fileSave)
+            {
+                this.nameFileSave = fileSave;
+            }
+
+            public void setFolder(string folder)
+            {
+                this.folder = folder;
+            }
+
+            public string getLocalPath()
+            {
+                return this.localPath;
+            }
+            public string getFileRemote()
+            {
+                return this.fileRemote;
+            }
+            public string getNameFileSave()
+            {
+                return this.nameFileSave;
+            }
+            public string getFolder()
+            {
+                return this.folder;
+            }
+
+            public void Baixar()
+            {
+                FtpWebRequest reqFTP;
+                
+                try
+                {
+
+                    FileStream outputStream = new FileStream(getLocalPath() + "\\" +
+                    getNameFileSave(), FileMode.Create);
+
+
+                    if (getFolder() == null)
+                    {
+                        //Abaixo deve ser passado o host no lugar de "seuhost" manualmente. será implementado arquivo conf.
+                        reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + "seuhost" + "/" + getFileRemote()));
+
+                    }
+                    else
+                    {   //Abaixo deve ser passado o host manualmente. será implementado arquivo conf.
+                        reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + "seuhost" + "/" + getFolder() + "/" + getFileRemote()));
+                    }
+
+                    reqFTP.Method = WebRequestMethods.Ftp.DownloadFile;
+
+
+                    reqFTP.UseBinary = true;
+
+                    /*
+                       O comando abaixo nessida de um host ftp. atualmente deve ser inserido manualmente.
+                     * Será criado arquivo de configuração para passar host e senha
+                     */
+                    reqFTP.Credentials = new NetworkCredential("seuhost", "senha");
+
+
+
+                    FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
+
+
+                    Stream ftpStream = response.GetResponseStream();
+
+
+                    long cl = response.ContentLength;
+
+
+
+                    int bufferSize = 4096;
+
+
+                    int readCount;
+
+
+                    byte[] buffer = new byte[bufferSize];
+
+
+
+                    readCount = ftpStream.Read(buffer, 0, bufferSize);
+
+
+                    while (readCount > 0)
+                    {
+                        outputStream.Write(buffer, 0, readCount);
+
+
+                        readCount = ftpStream.Read(buffer, 0, bufferSize);
+
+                    }
+                    if (readCount == 0)
+                    {
+
+                        MessageBox.Show("Parabéns, Download Concluído com Sucesso!", "Mensagem");
+
+                    }
+                    ftpStream.Close();
+
+
+                    outputStream.Close();
+
+
+                    response.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error!");
+
+                }
+            }
+    }
+}
+
